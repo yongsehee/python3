aa49702ca203bfdcd614b3a929ad3145c1412c0b
yongsehee <149376067+yongsehee@users.noreply.github.com>
2023-11-04 15:21:33 +0900

    Add files via upload

---
+++ 백준/1. 입출력과 사칙연산/곱셈.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = input() #문자열로 입력받기
+
+#472*5 출력
+axb0 = a*int(b[0])
+#472*8 출력
+axb1 = a*int(b[1])
+#472*3 출력
+axb2 = a*int(b[2])
+
+axb = a*int(b) #문자열 -> 정수로 변환
+
+print(axb2)
+print(axb1)
+print(axb0)
+print(axb)
