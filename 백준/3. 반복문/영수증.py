d0bbd03e2e77469350f46c358f2c7bf964763d46
yongsehee <149376067+yongsehee@users.noreply.github.com>
2023-11-14 21:49:01 +0900

    Add files via upload

---
+++ 백준/3. 반복문/영수증.py
@@ -0,0 +1,10 @@
+x = int(input())
+N = int(input())
+c = 0
+for i in range(N):
+    a,b = map(int,input().split())
+    c += (a*b)
+if x == c:
+    print('Yes')
+else:
+    print('No')
---
+++ 백준/3. 반복문/코딩은 체육과목 입니다.py
@@ -0,0 +1,3 @@
+n = int(input())
+x = n//4
+print('long ' * x, 'int', sep='')
